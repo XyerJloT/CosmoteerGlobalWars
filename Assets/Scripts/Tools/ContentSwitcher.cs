619d62d407ca56c666d21ed28e17cea74aeaf8d5
Chamber6821 <vania.osinin@yandex.ru>
2022-04-19 17:46:09 +0300

    feat: add tool class ContentSwitcher for menus

---
+++ Assets/Scripts/Tools/ContentSwitcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ContentSwitcher : MonoBehaviour
+{
+    [SerializeField] private int _defaultActiveTab;
+    [SerializeField] private GameObject[] _tabContainers;
+
+    public GameObject ActiveTab => _tabContainers[ActiveTabIndex];
+
+    public int ActiveTabIndex
+    {
+        get => _activeTab;
+        set
+        {
+            if (!(0 <= value && value <= _tabContainers.Length))
+            {
+                throw new ArgumentOutOfRangeException($"Всего {_tabContainers.Length} вкладок, а запросили вкладку {value}");
+            }
+
+            _tabContainers[_activeTab].SetActive(false); // Закрываем текущую вкладку
+            _tabContainers[value].SetActive(true); // Открываем новую
+
+            _activeTab = value;
+        }
+    }
+
+    private int _activeTab = 0;
+
+    private void Start()
+    {
+        foreach (var tab in _tabContainers)
+        {
+            tab.SetActive(false);
+        }
+
+        ActiveTabIndex = _defaultActiveTab;
+    }
+
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            ActiveTabIndex = _defaultActiveTab;
+        }
+    }
+}
