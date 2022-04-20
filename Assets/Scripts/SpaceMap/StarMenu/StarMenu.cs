a819705c83546168b657f9ef4ae7bbcd8eb4f6d7
Chamber6821 <vania.osinin@yandex.ru>
2022-04-20 22:41:10 +0300

    feat: use InflateAnimation in StarMenu

---
+++ Assets/Scripts/SpaceMap/StarMenu/StarMenu.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(InflateAnimation), typeof(Transform))]
+public class StarMenu : MonoBehaviour
+{
+    [SerializeField] private Text _starName;
+
+    [Header("Build Tab")]
+    [SerializeField] private Transform _blueprintContainer;
+    [SerializeField] private BlueprintRow _blueprintRowPrefab;
+
+    public StarView OpenedStar { get; private set; }
+
+    public bool IsOpened => OpenedStar != null;
+
+    private List<BlueprintRow> _blueprints = new List<BlueprintRow>();
+    private Vector3 _menuPositionOffset = new Vector3(100, -50);
+    private InflateAnimation _animation;
+    private Transform _myTransform;
+
+    public void Reopen(StarView target)
+    {
+        if (target == OpenedStar) return;
+
+        StopAllCoroutines();
+        StartCoroutine(ReopenCoroutine());
+
+        IEnumerator ReopenCoroutine()
+        {
+            if (IsOpened) yield return CloseCoroutine();
+            yield return OpenCoroutine(target);
+        }
+    }
+
+    public void Open(StarView target)
+    {
+        if (IsOpened) return;
+
+        StopAllCoroutines();
+        StartCoroutine(OpenCoroutine(target));
+    }
+
+    public void Close()
+    {
+        if (!IsOpened) return;
+
+        StopAllCoroutines();
+        StartCoroutine(CloseCoroutine());
+    }
+
+    private void Start()
+    {
+        _animation = GetComponent<InflateAnimation>();
+        _myTransform = GetComponent<Transform>();
+    }
+
+    private IEnumerator OpenCoroutine(StarView target)
+    {
+        _starName.text = target.Model.Name;
+        SetBlueptintCollection(target.Model.Blueprints);
+
+        MoveToStar(target.transform);
+        yield return _animation.OpenCoroutine();
+        
+        OpenedStar = target;
+
+        EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+
+    private IEnumerator CloseCoroutine()
+    {
+        yield return _animation.CloseCoroutine();
+        
+        _starName.text = "Empty";
+
+        RemoveBlueprintCollection(OpenedStar.Model.Blueprints);
+        OpenedStar = null;
+    }
+
+    private void HandleChangeBlueprintList(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        // TODO: Изменение списка чертежей вместо ReloadBlueprints
+        ReloadBlueprints(OpenedStar.Model.Blueprints);
+    }
+
+    private void ReloadBlueprints(IEnumerable<Blueprint> blueprints)
+    {
+        ClearBlueprints();
+
+        foreach (var blueprint in blueprints)
+        {
+            var row = InstantiateBlueprintRow(blueprint);
+            _blueprints.Add(row);
+        }
+    }
+
+    private void ClearBlueprints()
+    {
+        _blueprints.ForEach(Destroy);
+    }
+
+    private BlueprintRow InstantiateBlueprintRow(Blueprint blueprint)
+    {
+        var row = Instantiate(_blueprintRowPrefab, _blueprintContainer);
+
+        // TODO: Инициализировать BlueprintRow
+        
+        return row;
+    }
+
+    private void MoveToStar(Transform targetTransform)
+    {
+        _myTransform.localPosition = targetTransform.localPosition + _menuPositionOffset;
+    }
+
+    private void SetBlueptintCollection(ObservableCollection<Blueprint> blueprints)
+    {
+        blueprints.CollectionChanged += HandleChangeBlueprintList;
+        ReloadBlueprints(blueprints);
+    }
+
+    private void RemoveBlueprintCollection(ObservableCollection<Blueprint> blueprints)
+    {
+        blueprints.CollectionChanged -= HandleChangeBlueprintList;
+        ClearBlueprints();
+    }
+}
--- Assets/Scripts/Tools/InflateAnimation.cs
+++ Assets/Scripts/Tools/InflateAnimation.cs
@@ -2,64 +2,35 @@ using System;
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
 public class InflateAnimation : MonoBehaviour
 {
-    // Если нужно анимировать несколько объектов, сделай им общего объекта-родителя и анимируй его
-    [Tooltip("Объект который будет анимирован")]
-    [SerializeField] private Transform _animatedTransform;
-    
     [Tooltip("Длительность анимации в секундах")]
     [SerializeField, Min(0)] private float _animationDuration = 0.1f;
     
     [Tooltip("Количество кадров анимации")]
     [SerializeField, Min(1)] private int _frames = 10;
 
-    [Tooltip("Деактивировать объект пока закрыт")]
-    [SerializeField] private bool _willBeDeactivated = false;
-    
-    // Приватные сеттер, что бы случайно не изменили значения флага из другого скрипта
-    public bool IsOpened { get; private set; }
-
+    // Если нужно анимировать несколько объектов, сделай им общего объекта-родителя и анимируй его
+    private Transform _animatedTransform;
     private Vector3 _baseScale;
 
-    public void Open()
+    public IEnumerator OpenCoroutine()
     {
-        if (IsOpened) return;
-
-        StopAllCoroutines();
-        StartCoroutine(OpenAnimation());
-    }
-
-    public void Close()
-    {
-        if (!IsOpened) return;
-        
-        StopAllCoroutines();
-        StartCoroutine(CloseAnimation());
-    }
-    
-    private void Start()
-    {
-        _baseScale = _animatedTransform.localScale;
-        _animatedTransform.localScale = Vector3.zero;
-
-        if (_willBeDeactivated) _animatedTransform.gameObject.SetActive(false);
-    }
-
-    private IEnumerator OpenAnimation()
-    {
-        if (_willBeDeactivated) _animatedTransform.gameObject.SetActive(true);
         yield return ScaleAnimation(i => (i + 1) / _frames);
-
-        IsOpened = true;
     }
 
-    private IEnumerator CloseAnimation()
+    public IEnumerator CloseCoroutine()
     {
         yield return ScaleAnimation(i => 1 - (i + 1) / _frames);
-        if (_willBeDeactivated) _animatedTransform.gameObject.SetActive(false);
+    }
 
-        IsOpened = false;
+    private void Start()
+    {
+        _animatedTransform = GetComponent<Transform>();
+
+        _baseScale = _animatedTransform.localScale;
+        _animatedTransform.localScale = Vector3.zero;
     }
 
     private IEnumerator ScaleAnimation(Func<float, float> partFormula)
