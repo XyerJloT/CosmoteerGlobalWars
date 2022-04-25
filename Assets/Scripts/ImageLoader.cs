56095ac714988fc1e4391ac6585b6fff601657c7
Chamber6821 <vania.osinin@yandex.ru>
2022-04-25 21:49:46 +0300

    feat: add sync ImageLoader

---
+++ Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ImageLoader
+{
+    private Dictionary<string, Texture2D> _cache = new Dictionary<string, Texture2D>();
+
+    public Texture2D Load(string path)
+    {
+        if (_cache.ContainsKey(path) && File.GetLastWriteTime(path) < System.DateTime.Now)
+        {
+            return _cache[path];
+        }
+        
+        byte[] data = File.ReadAllBytes(path);
+
+        var texture = new Texture2D(0, 0);
+        texture.LoadImage(data);
+        
+        _cache[path] = texture;
+        return texture;
+    }
+}
