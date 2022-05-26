527c61f0882b2135a5815f67a3e5850cbfb684e1
Chamber6821 <vania.osinin@yandex.ru>
2022-05-26 19:01:33 +0300

    feat: add Tunnel and SpaceMap

---
+++ Assets/Scripts/Map/SpaceMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Scripts.Map
+{
+    public class SpaceMap
+    {
+        public readonly IEnumerable<Star> Stars;
+
+        private readonly Dictionary<Star, List<Tunnel>> _tunnelMap = new Dictionary<Star, List<Tunnel>>();
+
+        public SpaceMap(IEnumerable<Tunnel> tunnels)
+        {
+            Stars = tunnels.SelectMany(t => new Star[] { t.From, t.To }).Distinct().ToList();
+
+            foreach (var star in Stars)
+            {
+                _tunnelMap.Add(star, new List<Tunnel>());
+            }
+
+            foreach (var tunnel in tunnels)
+            {
+                _tunnelMap[tunnel.From].Add(tunnel);
+            }
+        }
+
+        public IEnumerable<Tunnel> GetTunnelsOnStar(Star star) => _tunnelMap[star];
+
+        public IEnumerable<Tunnel> AllTunnels() => _tunnelMap.Values.SelectMany(list => list);
+    }
+}
---
+++ Assets/Scripts/Map/Tunnel.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Map
+{
+    public class Tunnel
+    {
+        public readonly Star From;
+        public readonly Star To;
+
+        public Tunnel(Star from, Star to)
+        {
+            From = from;
+            To = to;
+        }
+    }
+}
