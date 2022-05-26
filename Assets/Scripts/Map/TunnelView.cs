71b352e8fc5bd642f37db852c9426cdd1776c0e8
Chamber6821 <vania.osinin@yandex.ru>
2022-05-26 19:06:56 +0300

    feat: add Tunnel prefab

---
+++ Assets/Scripts/Map/TunnelView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Map
+{
+    public class TunnelView : MonoBehaviour
+    {
+        public void Init(Tunnel tunnel)
+        {
+            var from = tunnel.From.Position;
+            var to = tunnel.To.Position;
+
+            var position = (from + to) / 2;
+            var angle = Vector2.Angle(from, to);
+
+            gameObject.transform.position = position;
+            
+            var rotation = gameObject.transform.eulerAngles;
+            rotation.z = angle;
+            gameObject.transform.eulerAngles = rotation;
+
+            //TODO: растягивание тоннеля
+        }
+    }
+}
